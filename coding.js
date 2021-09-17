d270b821a95f1c41202d63e1b51b43e315857c67
Reynald Ramirez <reynaldramirez@hotmail.com>
2021-09-16 20:27:29 -0400

    States managing snippet

---
+++ coding.js
@@ -0,0 +1,19 @@
+// React snippets:
+
+//# How to update an state when trying to get it as an Event:
+// 1st way
+useEffect(() => {
+    console.log(state);
+}, [state]);
+
+const handleEvent = e => {
+    setState(e.target.value);
+}
+// 2nd way
+const handleEvent = e => {
+    setState(e.target.value);
+}
+
+console.log(state);
+
+//# Explanation: https://gist.github.com/bpas247/e177a772b293025e5324219d231cf32c
