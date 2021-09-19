aa31fa2a75510cf3d03ca129c4ec6a024ca06eb6
Reynald Ramirez <reynaldramirez@hotmail.com>
2021-09-19 18:13:40 -0400

    Snippets

--- coding.js
+++ coding.js
@@ -17,3 +17,8 @@ const handleEvent = e => {
 console.log(state);
 
 //# Explanation: https://gist.github.com/bpas247/e177a772b293025e5324219d231cf32c
+
+// Github related snippets:
+
+//# How to merge unrelated histories
+//# Explanation: https://www.educative.io/edpresso/the-fatal-refusing-to-merge-unrelated-histories-git-error
